ef5c3e4ca45ae1af72b11c7659870ab23eb3d65a
Ofoe Fiergbor <ofoefiergbor16@gmail.com>
2020-04-11 21:25:44 +0000

    test 4

--- src/estimator.js
+++ src/estimator.js
@@ -33,18 +33,14 @@ const currentlyInfectedEst = (data) => {
 // #CHALLENGE TWO
 
 const severeCasesByRequestedTimeEst = (data) => {
-  const { timeToElapse } = data;
-
-  const powerFactor = Math.trunc(timeToElapse / 3);
+  const powerFactor = Math.trunc(data.timeToElapse / 3);
 
   impact.severeCasesByRequestedTime = Math.trunc(0.15 * impact.currentlyInfected * (2 ** powerFactor));
   severeImpact.severeCasesByRequestedTime = Math.trunc(0.15 * severeImpact.currentlyInfected * (2 ** powerFactor));
 // eslint-disable-next-line linebreak-style
 };
 const hospitalBedsByRequestedTimeEst = (data) => {
-  const { totalHospitalBeds } = data;
-
-  const bedAvailabilityInHospitals = 0.35 * totalHospitalBeds;
+  const bedAvailabilityInHospitals = 0.35 * data.totalHospitalBeds;
 
   impact.hospitalBedsByRequestedTime = Math.trunc(bedAvailabilityInHospitals - impact.severeCasesByRequestedTime);
   severeImpact.hospitalBedsByRequestedTime = Math.trunc(bedAvailabilityInHospitals - severeImpact.severeCasesByRequestedTime);
@@ -52,12 +48,12 @@ const hospitalBedsByRequestedTimeEst = (data) => {
 
 // #CHALLENGE THREE
 const casesForICUByRequestedTimeEst = () => {
-  impact.casesForICUByRequestedTime = Math.trunc(0.02 * impact.infectionsByRequestedTime);
-  severeImpact.casesForICUByRequestedTime = Math.trunc(0.02 * severeImpact.infectionsByRequestedTime);
+  impact.casesForICUByRequestedTime = Math.trunc(0.05 * impact.infectionsByRequestedTime);
+  severeImpact.casesForICUByRequestedTime = Math.trunc(0.05 * severeImpact.infectionsByRequestedTime);
 };
 const casesForVentilatorsByRequestedTimeEst = () => {
-  impact.casesForVentilatorsByRequestedTime = Math.trunc(0.05 * impact.infectionsByRequestedTime);
-  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(0.05 * severeImpact.infectionsByRequestedTime);
+  impact.casesForVentilatorsByRequestedTime = Math.trunc(0.02 * impact.infectionsByRequestedTime);
+  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(0.02 * severeImpact.infectionsByRequestedTime);
 };
 
 const dollarsInFlightEst = (data) => {
